845ef5110918bed90916fa7c70b0c593f85c3f62
GlitchBox <glitchbox29@gmail.com>
2020-05-30 14:47:16 +0600

    commented out all mysql codes, mongo db connection has been established. The app is broken because the routes no longer function

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 const defaultController = require('./controllers/defaultPage');
-const sequelize = require('./util/database');
-const Product = require('./models/product');
-const User = require('./models/user');
-const Cart = require('./models/cart');
-const CartItem = require('./models/cart-item');
-const Order = require('./models/order');
-const OrderItem = require('./models/oder-item');
+// const sequelize = require('./util/database');
+// const Product = require('./models/product');
+// const User = require('./models/user');
+// const Cart = require('./models/cart');
+// const CartItem = require('./models/cart-item');
+// const Order = require('./models/order');
+// const OrderItem = require('./models/oder-item');
+const mongoConnect = require('./util/database');
 
 const expressFunction = express();
 
@@ -44,73 +45,83 @@ expressFunction.use(express.static(path.join(__dirname, 'public')));
 //I'll assume every request has been made from the dummy user's accountexp
 expressFunction.use((request, response, next)=>{
 
-    User.findByPk(1).then(user=>{
-        //I can simply add a field to request object
-        request.user = user;
-        next();
-    }).catch(err=>{
-        console.log(err);
-    });
+    // User.findByPk(1).then(user=>{
+    //     //I can simply add a field to request object
+    //     request.user = user;
+    //     next();
+    // }).catch(err=>{
+    //     console.log(err);
+    // });
 });
 
 //routes that start with "/admin"
-expressFunction.use('/admin', adminRoutes);
-//routes that start with "/"
-expressFunction.use(shopRoutes);
+// expressFunction.use('/admin', adminRoutes);
+// //routes that start with "/"
+// expressFunction.use(shopRoutes);
 expressFunction.use("/", defaultController.notFound);
 
 //before I create and update tables(by using sync method), I'll establish relationships
 //the following line simply means a user creates an instance of Product
 //optional argument denotes that on the deletion of a User, the deletion
 //will be applied to any connected Product
-Product.belongsTo(User, {constraints: true, onDelete:'CASCADE'});
-User.hasMany(Product);
+// Product.belongsTo(User, {constraints: true, onDelete:'CASCADE'});
+// User.hasMany(Product);
 
-User.hasOne(Cart);
-Cart.belongsTo(User);
+// User.hasOne(Cart);
+// Cart.belongsTo(User);
 
-Cart.belongsToMany(Product, {through: CartItem});
-Product.belongsToMany(Cart, {through: CartItem});
+// Cart.belongsToMany(Product, {through: CartItem});
+// Product.belongsToMany(Cart, {through: CartItem});
 
-Order.belongsTo(User);
-User.hasMany(Order);
+// Order.belongsTo(User);
+// User.hasMany(Order);
 
-Order.belongsToMany(Product, {through: OrderItem});
-Product.belongsToMany(Order, {through: OrderItem});
+// Order.belongsToMany(Product, {through: OrderItem});
+// Product.belongsToMany(Order, {through: OrderItem});
 
-//npm start runs this whenever app.js is restarted
-//create all the defined(in models folder) tables
-//doesn't overwrite if the table already exists
+// //npm start runs this whenever app.js is restarted
+// //create all the defined(in models folder) tables
+// //doesn't overwrite if the table already exists
 
-// {force: true} options forces all the updates
-// sequelize.sync({force:true}).then(result=>{
-sequelize.sync().then(result=>{
+// // {force: true} options forces all the updates
+// // sequelize.sync({force:true}).then(result=>{
+// sequelize.sync().then(result=>{
     
-    return User.findByPk(1);
+//     return User.findByPk(1);
 
-}).then(user=>{
+// }).then(user=>{
     
-    if(!user){
-       return User.create({
-           name: 'Navid',
-           email: 'glitchbox29@gmail.com'
-       }); 
-    }
+//     if(!user){
+//        return User.create({
+//            name: 'Navid',
+//            email: 'glitchbox29@gmail.com'
+//        }); 
+//     }
 
-    return Promise.resolve(user);
+//     return Promise.resolve(user);
 
-})
+// })
 // .then(user=>{
     
 //    return user.createCart();
 
 // })
-.then(cart=>{
+// .then(cart=>{
+
+//     //expressFunction is called with every request
+//     const server = http.createServer(expressFunction);
+//     server.listen(port=6789, hostname="localhost");
+
+// }).catch(err=>{
+//     console.log(err);
+// });
+
+mongoConnect(client=>{
+    
+    console.log(client);
 
     //expressFunction is called with every request
     const server = http.createServer(expressFunction);
     server.listen(port=6789, hostname="localhost");
-
-}).catch(err=>{
-    console.log(err);
 });
+
--- util/database.js
+++ util/database.js
@@ -1,11 +1,30 @@
-const Sequelize = require('sequelize');
+//following is the code for connecting to mysql through sequelize
 
-const sequelize = new Sequelize('node-complete',
-                                'root',
-                                'BLEh-1234',
-                                {dialect: 'mysql',
-                                host: 'localhost'
-                            }
-);
+// const Sequelize = require('sequelize');
 
-module.exports = sequelize;
+// const sequelize = new Sequelize('node-complete',
+//                                 'root',
+//                                 'BLEh-1234',
+//                                 {dialect: 'mysql',
+//                                 host: 'localhost'
+//                             }
+// );
+
+// module.exports = sequelize;
+
+//following is the code for connecting to MongoDB
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = (callback)=>{
+    
+    MongoClient.connect('mongodb+srv://root:BLEh-1234@cluster0-vsvci.gcp.mongodb.net/test?retryWrites=true&w=majority')
+    .then(client=>{
+        console.log('Connection complete!');
+        callback(client);
+    }).catch(err=>{
+        console.log('error connecting to mongodb');
+    });
+};
+
+module.exports = mongoConnect; 
